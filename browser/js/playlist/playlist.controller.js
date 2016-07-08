aca0452ea650ea7de7cadd825b1fb293e27929c7
Richard Chao <r.chao03@gmail.com>
2016-07-08 18:25:16 -0400

    feelin kinda good

--- browser/js/playlist/playlist.controller.js
+++ browser/js/playlist/playlist.controller.js
@@ -1,38 +1,41 @@
 'use strict'
 
-juke.controller('PlaylistCtrl', function($scope, PlaylistFactory, thePlaylist, PlayerFactory) {
-
-  $scope.playlist = thePlaylist;
+juke.controller('NewPlaylistCtrl', function($scope, PlaylistFactory, PlayerFactory) {
 
   $scope.submitPlaylist = function() {
-        console.log("working")
         var data = {
             name: $scope.nameOfPlaylist
         };
 
         $scope.nameOfPlaylist = null;
         $scope.playlistForm.input.$setPristine();
+        $state.go('currentPlaylist');
         return PlaylistFactory.create(data);
     }
 
-  $scope.toggle = function (song) {
-    if (song !== PlayerFactory.getCurrentSong()) {
-      PlayerFactory.start(song, $scope.album.songs);
-    } else if ( PlayerFactory.isPlaying() ) {
-      PlayerFactory.pause();
-    } else {
-      PlayerFactory.resume();
-    }
-  };
-
-  $scope.getCurrentSong = function () {
-    return PlayerFactory.getCurrentSong();
-  };
-
-  $scope.isPlaying = function (song) {
-    return PlayerFactory.isPlaying() && PlayerFactory.getCurrentSong() === song;
-  };
-
 })
+
+juke.controller('SinglePlaylistCtrl', function($scope, thePlaylist) {
+  $scope.playlist = thePlaylist;
+
+  //   $scope.toggle = function (song) {
+  //   if (song !== PlayerFactory.getCurrentSong()) {
+  //     PlayerFactory.start(song, $scope.album.songs);
+  //   } else if ( PlayerFactory.isPlaying() ) {
+  //     PlayerFactory.pause();
+  //   } else {
+  //     PlayerFactory.resume();
+  //   }
+  // };
+
+  // $scope.getCurrentSong = function () {
+  //   return PlayerFactory.getCurrentSong();
+  // };
+
+  // $scope.isPlaying = function (song) {
+  //   return PlayerFactory.isPlaying() && PlayerFactory.getCurrentSong() === song;
+  // };
+
+});
 
 
--- browser/js/playlist/playlist.states.js
+++ browser/js/playlist/playlist.states.js
@@ -3,13 +3,13 @@ juke.config(function ($stateProvider) {
   $stateProvider.state('newPlaylist', {
     url: '/playlists/new',
     templateUrl: '/js/playlist/templates/playlist.html',
-    controller: 'PlaylistCtrl'
+    controller: 'NewPlaylistCtrl'
   });
 
     $stateProvider.state('singlePlaylist', {
     url: '/singlePlaylist/:playlistId',
     templateUrl: '/js/playlist/templates/singlePlaylist.html',
-    controller: 'PlaylistCtrl',
+    controller: 'SinglePlaylistCtrl',
     resolve: {
       thePlaylist: function(PlaylistFactory, $stateParams) {
         return PlaylistFactory.getPlaylist($stateParams.playlistId);
